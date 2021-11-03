5fcff0db95c9485359d63e2485461a24b9d73bfb
ayeonChoi <201120cay@dsm.hs.kr>
2021-11-03 23:01:54 +0900

    [ADD] chatUserModel

---
+++ src/models/chatUser.ts
@@ -0,0 +1,21 @@
+import Sequelize, { Model } from "sequelize";
+import { sequelize } from "../config/config";
+
+export class ChatUser extends Model {
+  roomId: number;
+  userEmail: string;
+}
+
+ChatUser.init(
+  {
+    chatUserId: {
+      type: Sequelize.NUMBER,
+      primaryKey: true,
+      field: "chat_user_id",
+    },
+    isBlock:{
+      type: Sequelize.BOOLEAN,
+    },
+  },
+  { sequelize, modelName: "chat_user", tableName: "chat_user" }
+);
