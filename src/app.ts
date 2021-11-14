67a2f05c9d441af9ab80fd4d2368f7711ccc70e9
ayeonChoi <201120cay@dsm.hs.kr>
2021-11-14 23:29:15 +0900

    [ADD]connect db

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,37 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 import cors from "cors";
 import http from "http";
-import socketio from "socket.io";
+import * as dotenv from "dotenv";
+import path from "path";
+import { sequelize } from "./config/config";
+
+dotenv.config({ path: path.join(__dirname + "../../.env") });
 
 const app: Application = express();
-
+// const io = socketio(server);
+const io = require("socket.io")(http);
 const server: http.Server = http.createServer(app);
-const io = socketio(server);
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+sequelize.sync();
+
+app.use((err, req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ message: err.message });
+});
+
 io.sockets.on("connection", (socket) => {
   console.log(1);
 });
-
 server.listen(5000, () => {
   console.log("server on!");
 });
-
 export default app;
+
+
+
+
+
